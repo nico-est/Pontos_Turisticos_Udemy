eb3841699cdc05c53c20bf84abedcc8ae0b923d0
Nicoest <nicolas.estanislau@gmail.com>
2021-02-18 10:02:18 -0300

    att

--- core/api/serializers.py
+++ core/api/serializers.py
@@ -3,14 +3,7 @@ from core.models import PontoTuristico
 from enderecos.models import Endereco
 
 
-class EnderecoSerializer(ModelSerializer):
-    class Meta:
-        model = Endereco
-        fields = ('cidade',)
-
-
 class PontoTuristicoSerializer(ModelSerializer):
-    endereco = EnderecoSerializer()
     class Meta:
         model = PontoTuristico
-        fields = ('id', 'nome', 'descricao', 'endereco')
+        fields = ('id', 'nome', 'descricao')
